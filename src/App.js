9e80bd6442c41e6047c72e5af211d1d8c483416a
matej <matej.basta@email.cz>
2022-03-07 10:15:46 +0100

    Step 6 finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import {Header} from "./components/Header.js";
+import {Footer} from "./components/Footer.js";
+import {ProductionList} from "./components/ProductionList.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="page">
+
+        <Header />
+
+        <div id="main">
+            <nav className="filter">
+                <label htmlFor="filter">Filter</label>
+                <input type="text" name="filter" id="filter" placeholder="enter search term" />
+            </nav>
+
+            <div className="content">
+                
+              <ProductionList />
+
+            </div>
+
+        </div>
+
+        <Footer />
+
     </div>
   );
 }
